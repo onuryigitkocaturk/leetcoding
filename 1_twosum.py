29a6ae48be3d97694e0d4903a3565f358464d8c3
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-08-06 16:12:31 +0300

    solve two sum problem

--- 1_twosum.py
+++ 1_twosum.py
@@ -1,23 +1,19 @@
-#brute force: time complexity->o(n^2)  space complexity->o(1)
-
 class Solution:
+    # brute force: time complexity->o(n^2)  space complexity->o(1)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i,j]
-        return[]
-    
+                    return [i, j]
+        return []
 
-#hash-map: time complexity->o(n) space complexity->o(n)
-
-class Solution:
+    # hash-map: time complexity->o(n) space complexity->o(n)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in seen:
-                return [seen[complement],i]
+                return [seen[complement], i]
             seen[nums[i]] = i
         return []
 
