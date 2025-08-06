63c99d9b8e59b5dedc23e3d64341955725cf3962
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-08-06 14:55:48 +0300

    two sum solved

---
+++ 1_twosum.py
@@ -0,0 +1,23 @@
+#brute force: time complexity->o(n^2)  space complexity->o(1)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return[]
+    
+
+#hash-map: time complexity->o(n) space complexity->o(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in seen:
+                return [seen[complement],i]
+            seen[nums[i]] = i
+        return []
+
