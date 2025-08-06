e8bf98af59b5a43d0b30f64c2531a25faae398c3
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-08-06 15:30:40 +0300

    containerwithmostwater solved

---
+++ 2_containerwithmostwater.py
@@ -0,0 +1,37 @@
+#brute force: time complexity:o(n^2), space complexity:o(1)
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+
+        for i in range(len(height)):
+            for j in range(i+1,len(height)):
+                h = min(height[i], height[j])
+                w = j - i
+
+                area = h * w
+                max_area = max(area, max_area)
+        return max_area
+    
+#two pointer: time complexity:0(n)
+
+class Solution:
+    def maxArea(self, height:[int]) -> int:
+        left_ptr = 0
+        right_ptr = len(height) - 1
+        max_area = 0
+
+        while right_ptr > left_ptr:
+            h = min(height[right_ptr],height[left_ptr])
+            w = right_ptr - left_ptr
+            area = h * w
+
+            max_area = max(area,max_area)
+
+            if(height[left_ptr]>height[right_ptr]):
+                right_ptr-=1
+            else:
+                left_ptr+=1
+        return max_area
+
+
+
