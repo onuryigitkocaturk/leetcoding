77066b7659d18d999bc8d1d070eac41ff014f118
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-08-10 22:47:25 +0300

    solve top k frequent elements problem

---
+++ 9_topkfrequentelements.py
@@ -0,0 +1,23 @@
+class Solution:
+    # time complexity:o(nlogn), space complexity:o(n+k)
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter_hash_map = {}
+        for i in nums:
+            if i not in counter_hash_map:
+                counter_hash_map[i] = 1
+            else:
+                counter_hash_map[i] += 1
+
+        sorted_hash_map = sorted(
+            counter_hash_map.items(), key=lambda x: x[1], reverse=True
+        )
+
+        result = []
+
+        for i in range(k):
+            key = sorted_hash_map[i][0]
+
+            result.append(key)
+
+        return result
