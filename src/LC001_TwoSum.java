8b97b691edb228914c2267c9f0c4cfbf10886df6
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-11-27 15:50:02 +0300

    Solve two sum problem

---
+++ src/LC001_TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC001_TwoSum {
+
+    public int[] twoSumBruteForce(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> valueToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (valueToIndex.containsKey(complement)) {
+                return new int[] {valueToIndex.get(complement), i};
+            }
+            valueToIndex.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
