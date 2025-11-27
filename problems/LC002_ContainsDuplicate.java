7f14e646bdf567b6eeb2a2810ed0b5d212fa3299
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-11-27 18:21:56 +0300

    Solve contains duplicate problem

---
+++ problems/LC002_ContainsDuplicate.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+public class LC002_ContainsDuplicate {
+
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
+    public boolean containsDuplicateBruteForce(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> checkList = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (checkList.contains(nums[i])) {
+                return true;
+            }
+            checkList.add(nums[i]);
+        }
+        return false;
+    }
+}
