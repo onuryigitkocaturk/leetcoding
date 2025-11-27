c1d323b64bd80e48075c399a694bb7abb623e9a2
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-11-27 18:21:04 +0300

    Add time and space complexity

--- problems/LC001_TwoSum.java
+++ problems/LC001_TwoSum.java
@@ -3,6 +3,8 @@ import java.util.Map;
 
 public class LC001_TwoSum {
 
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
     public int[] twoSumBruteForce(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
@@ -16,6 +18,8 @@ public class LC001_TwoSum {
         return new int[0];
     }
 
+    // Time complexity: O(n)
+    // Space complexity: O(n)
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> valueToIndex = new HashMap<>();
 
