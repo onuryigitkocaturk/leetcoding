f49225022ce26bda54aef4dbabdc4ac0563fb47c
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-12-08 14:57:58 +0300

    Solve interleaving string problem

---
+++ problems/LC010_InterleavingString.java
@@ -0,0 +1,32 @@
+public class LC010_InterleavingString {
+
+        // Time complexity: O(n x m )
+        // Space complexity: O(n x m)
+        public boolean isInterleave(String s1, String s2, String s3) {
+            int n = s1.length();
+            int m = s2.length();
+
+            if (s3.length() != n + m) {
+                return false;
+            }
+
+            boolean[][] dp = new boolean[n + 1][m + 1];
+            dp[0][0] = true;
+
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j <= m; j++) {
+
+                    if (i > 0 && dp[i - 1][j]
+                            && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                        dp[i][j] = true;
+                    }
+
+                    if (j > 0 && dp[i][j - 1]
+                            && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                        dp[i][j] = true;
+                    }
+                }
+            }
+            return dp[n][m];
+        }
+}
