2806993b2cd9e1ddcb49279ce72363c2eade8581
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2025-12-17 08:34:57 +0300

    Solve greatest common divisor of strings problem

---
+++ problems/LC014_GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,24 @@
+public class LC014_GreatestCommonDivisorOfStrings {
+
+    // Time complexity: O(n+m)
+    // Space complexity: O(1)
+    public String gcdOfStrings(String str1, String str2) {
+
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int gcdLen = gcd(str1.length(), str2.length());
+        return str1.substring(0,gcdLen);
+    }
+
+    private int gcd(int a, int b) {
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
